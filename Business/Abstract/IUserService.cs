9e999fdaabbe1d3c7345a11d2dabd1a2a971323b
Kerem Candangil <keremc4306@gmail.com>
2021-04-07 20:03:09 +0300

    End of the 14th day

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IResult AddUser(User user);
+        IDataResult<User> GetByMail(string email); // daha önce bir kullanıcı olup olmadığını tespit etmek için gerekli iş kodu => çözmem gerek :(
+
+        IDataResult<List<OperationClaim>> GetClaims(User user);
+    }
+}
